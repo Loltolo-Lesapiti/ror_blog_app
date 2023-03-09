5ddfd84369ae9214f6b8fbeb3417269ad4dcfeea
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2023-03-09 16:20:33 +0300

    Update db migrations

--- db/migrate/20221207123331_create_posts.rb
+++ db/migrate/20221207123331_create_posts.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :text
-      t.integer :comment_counter
-      t.integer :likes_counter
+      t.text :text
+      t.integer :likes_counter, default: 0
+      t.integer :comments_counter, default: 0
 
       t.timestamps
     end
--- db/migrate/20221207124249_create_comments.rb
+++ db/migrate/20221207124249_create_comments.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :text
-
+      t.text :text
       t.timestamps
     end
   end
