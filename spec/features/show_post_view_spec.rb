c851ccaf0424077605aef2c5a274390e64a7f228
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2023-03-14 09:11:50 +0300

    Update specs

--- spec/features/show_post_view_spec.rb
+++ spec/features/show_post_view_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe 'Post index details page', type: :system do
-  describe 'testing for user post index page', js: true do
+  describe 'testing for user post index page' do
     it 'should display the title of the post' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       expect(page).to have_content('Imposter Syndrome')
     end
 
     it 'should displays the user profile.' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       expect(page).to have_content('Lilly')
     end
 
     it 'should see how many comments a post has.' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       expect(page).to have_content('Comments counter: 1')
     end
 
     it 'should see how many likes a post has.' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       expect(page).to have_content('Likes counter: 2')
     end
 
     it 'should see some of the post body' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       expect(page).to have_content('Title: Imposter Syndrome')
     end
 
     it 'should see the username of each commentor.' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       expect(page).to have_content('Lilly')
     end
 
     it 'should see the comment each commentor.' do
-      visit('/users/1/posts/1')
+      visit(user_post_path(@user, @post1))
       page.has_content?('Lilly: I would love to attend that school.')
     end
   end
--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -1,42 +1,53 @@
 require 'rails_helper'
+
 RSpec.describe 'User Show Page', type: :system do
-  describe 'User show page', js: true do
-    it 'should displays the the profile picture of user.' do
-      visit('users/1')
+  describe 'User show page' do
+    let(:user) { FactoryBot.create(:user) }
+
+    it 'should display the profile picture of the user.' do
+      visit(user_path(user))
       page.has_content?('Screenshot Image')
       expect(page).to have_css('img')
     end
-    it 'should displays the user profile.' do
-      visit('users/1')
-      expect(page).to have_content('Lilly')
+
+    it 'should display the user profile.' do
+      visit(user_path(user))
+      expect(page).to have_content(user.name)
     end
+
     it 'should display the number of posts per user.' do
-      visit('users/1')
-      expect(page).to have_content('3')
+      visit(user_path(user))
+      expect(page).to have_content(user.posts.count)
     end
+
     it 'should display the user bio' do
-      visit('users/1')
-      expect(page).to have_content('web developer')
+      visit(user_path(user))
+      expect(page).to have_content(user.bio)
     end
-    it 'should display first three posts by a user' do
-      visit('users/1')
-      expect(page).to have_content('Imposter Syndrome')
-      expect(page).to have_content('Microverse')
-      expect(page).to have_content('Hackarank')
+
+    it 'should display the first three posts by a user' do
+      visit(user_path(user))
+      user.posts.limit(3).each do |post|
+        expect(page).to have_content(post.title)
+      end
     end
+
     it 'should display a button to see all posts.' do
-      visit('users/1')
+      visit(user_path(user))
       expect(page).to have_content('See all posts')
     end
-    it 'should redirects to the post details when clicked' do
-      visit('users/1')
+
+    it 'should redirect to the post details when clicked' do
+      visit(user_path(user))
       click_button('See all posts')
-      expect(page).to have_current_path('/users/1/posts')
+      expect(page).to have_current_path(user_posts_path(user))
     end
-    # it 'it When I click a users post, it redirects me to that post' do
-    #   visit('users/1')
-    #   click_button('See all posts')
-    #   expect(page).to have_current_path('/users/1/posts')
-    # end
+
+    it 'should redirect to the clicked post' do
+      post = user.posts.first
+      visit(user_posts_path(user))
+      click_link(post.title)
+      expect(page).to have_current_path(user_post_path(user, post))
+    end
   end
 end
