92c25c13c60e34bdcf89f9582f902deebfc88dcc
FuadNabi <fuad.nebiyev95@mail.ru>
2023-03-14 11:51:06 +0400

    Fix lintern errors

--- spec/features/index_post_view_spec.rb
+++ spec/features/index_post_view_spec.rb
@@ -49,53 +49,53 @@ RSpec.describe 'Post index view', type: :system do
 
   describe 'Testing for user post index view', js: true do
     it 'should display the profile picture of the user.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       page.has_content?('Screenshot Image')
       expect(page).to have_css('img')
     end
 
     it 'should display the user name.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       expect(page).to have_content(@user.name)
     end
 
     it 'should display the number of posts by a user.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       page.has_content?("Number of posts: #{@user.posts_counter}")
     end
 
     it 'should display the title of the post' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       expect(page).to have_content(@post1.title.to_s)
     end
 
     it 'should see some text of the post body' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       expect(page).to have_content(@post1.text.to_s)
     end
 
     it 'should see the title text' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       expect(page).to have_content('Imposter Syndrome')
     end
 
     it 'should see the number of comments per post.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       page.has_content?("Comments counter: #{@post1.comments_counter}")
     end
 
     it 'should see the number of likes per post.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       page.has_content?("Likes counter: #{@post1.likes_counter}")
     end
 
     it 'should see a section for pagination if there are more posts than fit on the view.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       page.has_button?('Pagination')
     end
 
     it 'When I click on a post, it should redirects me to that post show page.' do
-      visit user_posts_path(@user,  @post1)
+      visit user_posts_path(@user, @post1)
       click_on(@post1.title)
       expect(page).to have_current_path user_post_path(@post1.author_id, @post1)
     end
